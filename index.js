ef0b199baa6d869e88871d26ab6a15413e009ce5
Devangend <80990706+Devangend@users.noreply.github.com>
2021-09-04 09:04:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 require('discord-buttons')(client);
+const { token } = require("../config.json");
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
@@ -10,5 +11,5 @@ require("dotenv").config();
 require("./Handlers/Commands.js")(client);
 require("./Handlers/Events.js")(client);
 
-client.login(process.env.TOKEN);
+client.login(token);
 
