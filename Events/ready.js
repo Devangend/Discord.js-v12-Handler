279f5f325eee94d7b9f0a05b177291f32c193bca
Devan <80990706+Devangend@users.noreply.github.com>
2021-12-20 03:24:12 +0800

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,3 +1,10 @@
 module.exports = async (client) => {
-  console.log(`${client.user.tag} is ready`);
+  
+  console.log(`
+Aplication is ready!
+Name: ${client.user.tag}
+Server: ${client.guilds.cache.size}
+Members: ${client.guilds.cache.map((g) => g.memberCount || 0).reduce((x, y) => x + y, 0)}
+`);
+  
 }
