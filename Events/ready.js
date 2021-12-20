19004ba782db1495d3fdb33a1b1a0a9ac4058b18
Devan <80990706+Devangend@users.noreply.github.com>
2021-12-20 08:04:36 +0800

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -6,6 +6,7 @@ Name: ${client.user.tag}
 Server: ${client.guilds.cache.size}
 Members: ${client.guilds.cache.map((g) => g.memberCount || 0).reduce((x, y) => x + y, 0)}
 ---------------------`);
+  
   // Custom Bot Presence Status
   client.user.setPresence({
     status: "online",  // You can show online, idle, and dnd
