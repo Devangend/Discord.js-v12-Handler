670a82e9a117f29195c19589bde320b8f81a676a
Devan <80990706+Devangend@users.noreply.github.com>
2021-12-20 08:04:27 +0800

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,11 +1,11 @@
 module.exports = async (client) => {
   
-  console.log(`
-Aplication is ready!
+  console.log(`---------------------
+Aplication is ready! 
 Name: ${client.user.tag}
 Server: ${client.guilds.cache.size}
 Members: ${client.guilds.cache.map((g) => g.memberCount || 0).reduce((x, y) => x + y, 0)}
-`);
+---------------------`);
   // Custom Bot Presence Status
   client.user.setPresence({
     status: "online",  // You can show online, idle, and dnd
