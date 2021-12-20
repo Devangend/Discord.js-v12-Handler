a151302302dc7e04c0626acef7c842511c30e02a
Devan <80990706+Devangend@users.noreply.github.com>
2021-12-20 08:00:00 +0800

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -6,5 +6,13 @@ Name: ${client.user.tag}
 Server: ${client.guilds.cache.size}
 Members: ${client.guilds.cache.map((g) => g.memberCount || 0).reduce((x, y) => x + y, 0)}
 `);
-  
+  // Custom Bot Presence Status
+  client.user.setPresence({
+    status: "online",  // You can show online, idle, and dnd
+    activity: {
+        name: "yo Waddap",  // The message shown
+        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+    }
+});
+});
 }
